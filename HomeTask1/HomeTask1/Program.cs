d275b2c9db17316dbbcc4ed81c0081ff4fd8af93
Student <student@naa.local>
2025-10-24 09:17:20 +0400

    Task1

---
+++ HomeTask1/HomeTask1/Program.cs
@@ -0,0 +1,31 @@
+﻿using HomeTask1__;
+
+UsersService service = new UsersService();
+
+User[] allUsers = service.GetAll();
+Console.WriteLine("Istifadəçilər:");
+for (int i = 0; i < allUsers.Length; i++)
+{
+    Console.WriteLine(allUsers[i]);
+}
+
+Console.WriteLine("______________________________________________");
+int deleteId = 2;
+service.Delete(deleteId, out bool deleted);
+Console.WriteLine($"User silindi? {deleted}");
+
+
+Console.WriteLine("______________________________________________");
+User newUser = new User(17, "Seyid", "seyid@mail.com");
+service.Create(ref newUser, out bool created);
+Console.WriteLine($"Yeni user əlavə olundu? {created}");
+
+
+Console.WriteLine("______________________________________________");
+Console.WriteLine("Son veziyyet:");
+allUsers = service.GetAll();
+for (int i = 0; i < allUsers.Length; i++)
+{
+    Console.WriteLine(allUsers[i]);
+}
+
